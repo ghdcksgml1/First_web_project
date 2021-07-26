074f4f1afccbffbc12f43568af1bda9ccdc9c335
ghdcksgml1 <ghdcksgml2@naver.com>
2021-07-27 00:14:16 +0900

    2021.07.27

---
+++ js/all.js
@@ -0,0 +1,13 @@
+window.onload=()=>{
+    const meContent = document.querySelector('#meContent');
+
+    const mePostBtn = document.querySelector('#mePostBtn');
+
+    mePostBtn.addEventListener('click',()=>{
+        if(meContent.value == ''){
+            alert("내용을 입력하세요.");
+            meContent.focus();
+            return false;
+        }
+    });
+}
