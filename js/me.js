2830c0b992d3b044fb7e455d7918e4b1b538ce1e
ghdcksgml1 <ghdcksgml2@naver.com>
2021-08-30 01:05:47 +0900

    게시물 추가로 불러오기 기능 완성

--- js/me.js
+++ js/me.js
@@ -1,4 +1,3 @@
-window.onload = function(){
     const meContent = document.querySelector('#meContent');
     const mePostBtn = document.querySelector('#mePostBtn');
 
@@ -19,10 +18,9 @@ window.onload = function(){
 
     mePostBtn.addEventListener('click',()=>{
         const url = './database/contents.php';
-        // const json_file = {method:'POST',headers:{'Content-Type': 'application/json'},
-        // body:JSON.stringify({mode:"save",meContent: document.querySelector('#meContent').value})};
-        const json_file = {method:'POST',headers:{'Content-Type':'application/json'},
-                        body:JSON.stringify({mode:'loadMore',contentsLoadType:'me',page: 1})};
+        const json_file = {method:'POST',headers:{'Content-Type': 'application/json'},
+        body:JSON.stringify({mode:"save",meContent: document.querySelector('#meContent').value})};
+    
         fetch(url,json_file)
             .then(res=>{return res.json();})
             .then(data=>{
@@ -33,27 +31,45 @@ window.onload = function(){
                     alert('게시물 등록이 실패했습니다.');
                 }});
     });
+
+function debounce(callback,limit){
+    console.log("1");
+    let timeout
+    return function(...args){
+        clearTimeout(timeout);
+        timeout = setTimeout(()=>{
+            callback.apply(this,args);
+        }, limit);
+    }
 }
 
     // 스크롤 80% 도달 시 게시물 더 불러오기 기능 구현
-    window.addEventListener('scroll',throttle(()=>{
+    window.addEventListener('scroll',debounce(()=>{
         const page_num = document.querySelector('#page');
         const nowScroll = window.scrollY;
         const allScroll = document.body.scrollHeight;
 
-        if(page_num.value === 0){
+        if(page_num.value === '0'){
             return false;
         }
-
+        
         const nowPercent = nowScroll / allScroll * 100;
 
         if(nowPercent >= 80){
-            console.log("80 percent!!");
-            const url = '/database/contents.php';
+            // const url = '/database/contents.php';
+            // const json_file = {method:'POST',headers:{'Content-Type':'application/json'},
+            //             body:JSON.stringify({mode:'loadMore',contentsLoadType:'me',page: page_num.value})};
+            // fetch(url,json_file)
+            //     .then(res=>{return res.json;})
+            //     .then(data=>{
+            const page_val = page_num.value;
+            const url = './database/contents.php';
+            // const json_file = {method:'POST',headers:{'Content-Type': 'application/json'},
+            // body:JSON.stringify({mode:"save",meContent: document.querySelector('#meContent').value})};
             const json_file = {method:'POST',headers:{'Content-Type':'application/json'},
-                        body:JSON.stringify({mode:'loadMore',contentsLoadType:'me',page: page_num.value})};
+                            body:JSON.stringify({mode:'loadMore',contentsLoadType:'me',page: page_val})};
             fetch(url,json_file)
-                .then(res=>{return res.json;})
+                .then(res=>{return res.json();})
                 .then(data=>{
                     if(data.result===true){
                         const content = data.content;
@@ -64,41 +80,53 @@ window.onload = function(){
                         }else{
                             page_num.value = parseInt(page_num.value)+1;
                         }
+                        
+                        
+                        for(let contents in content){
+                            let d = new Date(content[contents]['regTime'] * 1000);
+                            let month = d.getMonth()+1;
+                            let regTime = d.getFullYear()+'년 '+month+'월 '+d.getDate()+'일 '+d.getHours()+'시 '+d.getMinutes()+'분';
+                            
+                            // 특수기호를 HTML코드로 변경 
+                            let bbs = content[contents]['content'];
+                            bbs = bbs.replace(/</g,'&lt;');
+                            bbs = bbs.replace(/>/g,'$gt;');
 
-                        center.innerHTML += `<div class="reading">
-                        <div class="writerArea">
-                            <img src="<?= $mc['profilePhoto'] ?>" />
-                            <div class="writingInfo">
-                                <p><?= $mc['userName'] ?></p>
-                                <div class="writingData"><?= date('Y년 m월 d일 H시 i분', $mc['regTime']) ?></div>
+                            center.innerHTML += `<div class="reading">
+                            <div class="writerArea">
+                                <img src="${content[contents]['profilePhoto']}" />
+                                <div class="writingInfo">
+                                    <p>${content[contents]['userName']}</p>
+                                    <div class="writingData">${regTime}</div>
+                                </div>
                             </div>
-                        </div>
-                        <span class="content"><?= nl2br(htmlspecialchars($mc['content'])) ?></span>
+                            <span class="content">${bbs}</span>
 
 
-                        <div class="likeArea">
-                            <div class="likeNum likes<?= $mc['contentsID'] ?>" style="background:#fff">공감 수: 250</div>
-                            <div class="likeBtn" id="likes<?= $mc['contentsID'] ?>">공감하기</div>
-                            <div class="contentsID">콘텐츠 번호: <?= $mc['contentsID'] ?></div>
-                        </div>
-
-                        <div class="myCommentArea myCommentArea861225">
-                            <div class="commentBox">
-                                <img src="<?= $_SESSION['myMemberSes']['profilePhoto'] ?>" />
-                                <p class="commentRegTime">2013년 12월 25일</p>
-                                <p class="commentPoster">홍찬희</p>
-                                <p class="writtenComment">정말 반갑습니다.</p>
+                            <div class="likeArea">
+                                <div class="likeNum likes 861225" style="background:#fff">공감 수: 250</div>
+                                <div class="likeBtn" id="likes 861225">공감하기</div>
+                                <div class="contentsID">콘텐츠 번호: 861225</div>
                             </div>
-                        </div>
-                        <div class="inputBox">
-                            <img src="<?= $_SESSION['myMemberSes']['profilePhoto'] ?>" />
-                            <input type="text" class="inputComment comments861225" placeholder="코멘트 입력" />
-                            <div class="regCommentBox">
-                                <input type="button" class="regCommentBtn" id="comments861225" value="게시" />
+
+                            <div class="myCommentArea myCommentArea861225">
+                                <div class="commentBox">
+                                    <img src="./images/me/happyCat.png" />
+                                    <p class="commentRegTime">2013년 12월 25일</p>
+                                    <p class="commentPoster">홍찬희</p>
+                                    <p class="writtenComment">정말 반갑습니다.</p>
+                                </div>
                             </div>
-                        </div>
-                    </div>`;
+                            <div class="inputBox">
+                                <img src="${content[contents]['profilePhoto']}" />
+                                <input type="text" class="inputComment comments${content[contents]['contentsID']}" placeholder="코멘트 입력" />
+                                <div class="regCommentBox">
+                                    <input type="button" class="regCommentBtn" id="comments${content[contents]['contentsID']}" value="게시" />
+                                </div>
+                            </div>
+                        </div>`;
+                        }
                     }
                 })
         }
-    },300));
+    },2000));
